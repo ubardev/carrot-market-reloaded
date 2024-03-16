a059205ea372fedd2b269c50859002528d9980b9
ubardev <bellin83@gmail.com>
2024-03-17 03:33:47 +0900

    :lipstick: #3.10 JIT

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      margin: {
+        good: '120px',
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
