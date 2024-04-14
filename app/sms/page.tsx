9dd6e4120d503724344ff1f587a80cccd541fa63
ubardev <bellin83@gmail.com>
2024-04-15 08:02:00 +0900

    :necktie: #6.8 SMS Validation

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -6,8 +6,13 @@ import SocialLogin from '@/components/social-login';
 import { useFormState } from 'react-dom';
 import { smsLogin } from '@/app/sms/actions';
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsLogin, null);
+  const [state, dispatch] = useFormState(smsLogin, initialState);
 
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -16,16 +21,25 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="text" placeholder="Phone number" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-          min={100000}
-          max={999999}
-        />
-        <Button text="Verify" />
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? 'Verify Token' : 'Send Verification SMS'} />
       </form>
       <SocialLogin />
     </div>
