805a1da5e32a9f039250a7a8d995d8ffb1caeead
ubardev <bellin83@gmail.com>
2024-05-28 07:36:22 +0900

    :necktie: #8.6 superRefine

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,18 +1,17 @@
 'use server';
-
 import bcrypt from 'bcrypt';
-import { z } from 'zod';
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from '@/lib/constants';
 import db from '@/lib/db';
-import { getIronSession } from 'iron-session';
-import { cookies } from 'next/headers';
+import { z } from 'zod';
 import { redirect } from 'next/navigation';
+import getSession from '@/lib/session';
 
 const checkUsername = (username: string) => !username.includes('potato');
+
 const checkPasswords = ({
   password,
   confirm_password,
@@ -21,32 +20,6 @@ const checkPasswords = ({
   confirm_password: string;
 }) => password === confirm_password;
 
-const checkUniqueUsername = async (username: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      username,
-    },
-    select: {
-      id: true,
-    },
-  });
-
-  return !Boolean(user);
-};
-
-const checkUniqueEmail = async (email: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      email,
-    },
-    select: {
-      id: true,
-    },
-  });
-
-  return !Boolean(user);
-};
-
 const formSchema = z
   .object({
     username: z
@@ -54,24 +27,52 @@ const formSchema = z
         invalid_type_error: 'Username must be a string!',
         required_error: 'Where is my username???',
       })
+      .toLowerCase()
       .trim()
-      .toLowerCase()
-      // .transform((username) => `🔥 ${username}`)
-      .refine(checkUsername, 'No potatos allowed!')
-      .refine(checkUniqueUsername, 'This username is already taken'),
-    email: z
-      .string()
-      .email()
-      .toLowerCase()
-      .refine(
-        checkUniqueEmail,
-        'There is an account already registered with that email. ',
-      ),
-    password: z
-      .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+      // .transform((username) => `🔥 ${username} 🔥`)
+      .refine(checkUsername, 'No potatoes allowed!'),
+    email: z.string().email().toLowerCase(),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    //.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
+  })
+  .superRefine(async ({ username }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        username,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'This username is already taken',
+        path: ['username'],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
+  })
+  .superRefine(async ({ email }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'This email is already taken',
+        path: ['email'],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
   })
   .refine(checkPasswords, {
     message: 'Both passwords should be the same!',
@@ -85,14 +86,12 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get('password'),
     confirm_password: formData.get('confirm_password'),
   };
-
-  const result = await formSchema.safeParseAsync(data);
-
+  const result = await formSchema.spa(data);
   if (!result.success) {
+    console.log(result.error.flatten());
     return result.error.flatten();
   } else {
     const hashedPassword = await bcrypt.hash(result.data.password, 12);
-
     const user = await db.user.create({
       data: {
         username: result.data.username,
@@ -103,14 +102,8 @@ export async function createAccount(prevState: any, formData: FormData) {
         id: true,
       },
     });
-
-    const session = await getIronSession(cookies(), {
-      cookieName: 'delicious-karrot',
-      password: process.env.COOKIE_PASSWORD!,
-    });
-
-    //@ts-ignore
-    cookie.id = user.id;
+    const session = await getSession();
+    session.id = user.id;
     await session.save();
     redirect('/profile');
   }
